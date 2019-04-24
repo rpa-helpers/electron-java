0f8948eeeaedb14bacf000a0bfa06d3d4b98bd96
Hrishabh Kumar <hrishabh.kumar@ipsoft.com>
2019-04-24 19:52:40 +0530

    Promisified Module to get java.

--- findJavaHome.js
+++ findJavaHome.js
@@ -1,10 +1,7 @@
-require('locate-java-home').default({
-  version: '>=1.8',
-  mustBe64Bit: true,
-}, function(err, home){
+require('find-java-home')(function(err, home){
   if(err){
     console.error("[node-java] "+err);
     process.exit(1);
   }
-  process.stdout.write(home[0].path);
+  process.stdout.write(home);
 });
